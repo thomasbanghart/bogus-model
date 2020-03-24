6ab3ca7406dce0c6cb33439a92fa45e42f44fce2
Noah MacDonald <noah.macdonald@looker.com>
2020-03-24 21:38:48 +0000

    schema viewer addition

---
+++ manifest.lkml
@@ -0,0 +1,13 @@
+project_name: "bogus"
+
+# # Use local_dependency: To enable referencing of another project
+# # on this instance with include: statements
+#
+# local_dependency: {
+#   project: "name_of_other_project"
+# }
+visualization: {
+  id: "bogus-schema-viewer"
+  label: "Schema Viewer"
+  file: "schemaviewer.js"
+}
---
+++ schemaviewer.js
@@ -0,0 +1,12 @@
+const visObject = {
+  options: {
+  },
+	create: function(element, config){
+	},
+	updateAsync: function(data, element, config, queryResponse, details, doneRendering){
+    element.innerHTML = '<iframe width="100%" height="100%" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRwheSFu4KOwxV1goMhzY13BtZrCkWJfHqziIgAadkynp4SQfQn7x0_N_p60YMV2O9Oo_xGxp4LwRRR/pubhtml?widget=true&amp;headers=false"></iframe>';
+  	doneRendering()
+	}
+};
+
+looker.plugins.visualizations.add(visObject);
