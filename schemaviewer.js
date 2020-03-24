8ef52b43af0f03b7dbcdde67693eed071bbf2c21
Noah MacDonald <noah.macdonald@looker.com>
2020-03-24 21:57:47 +0000

    hh

--- schemaviewer.js
+++ schemaviewer.js
@@ -4,7 +4,7 @@ const visObject = {
   create: function(element, config){
   },
   updateAsync: function(data, element, config, queryResponse, details, doneRendering){
-    element.innerHTML = '<iframe width="100%" height="100%" src="https://docs.google.com/spreadsheets/d/1e38QL47sD8Kub50WxxR5_UtDiXN_mt2ZQlHTMqax7YI/edit?usp=sharing"></iframe>';
+    element.innerHTML = '<iframe width="100%" height="100%" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRwheSFu4KOwxV1goMhzY13BtZrCkWJfHqziIgAadkynp4SQfQn7x0_N_p60YMV2O9Oo_xGxp4LwRRR/pubhtml?widget=true&amp;headers=false"></iframe>';
     doneRendering()
   }
 };
