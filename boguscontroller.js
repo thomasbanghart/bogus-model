bb0c9aacd7662f203f3742473d66cdc69c476d71
Noah MacDonald <noah.macdonald@looker.com>
2020-03-25 01:40:58 +0000

    commit please

--- boguscontroller.js
+++ boguscontroller.js
@@ -6,23 +6,23 @@ looker.plugins.visualizations.add({
   create: function(element, config) {
     element.innerHTML = `
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-		<style>
-			.dataset_input {
+    <style>
+      .dataset_input {
          display: block;
       }
-			.disp_input {
+      .disp_input {
          display: block;
       }
-			.btn-primary {
+      .btn-primary {
          display: block;
       }
-			.n_input {
+      .n_input {
          width: 80;
       }
-			.modelbogus_input {
-				display: block;
-			}
-		</style>`
+      .modelbogus_input {
+        display: block;
+      }
+    </style>`
 
     var container = element.appendChild(document.createElement("div"));
     container.className = 'viz-inspector'
@@ -30,17 +30,17 @@ looker.plugins.visualizations.add({
     var filename = element.appendChild(document.createElement("input"));
     filename.placeholder = `Bogus Filename`
     filename.className = 'filename_input'
-    
+
     var dataset = element.appendChild(document.createElement("input"));
     dataset.placeholder = `Dataset Name`
     dataset.className = 'dataset_input'
-    
+
     var writedisp = element.appendChild(document.createElement("select"));
     writedisp.innerHTML = `
       <option value="w">Drop & Replace</option>
       <option value="w+">Append</option>`
     writedisp.className = 'disp_input'
-    
+
     var modelbogus = element.appendChild(document.createElement("textarea"));
     modelbogus.rows = "4"
     modelbogus.placeholder = `{
@@ -56,13 +56,13 @@ looker.plugins.visualizations.add({
     label1.className = 'add_label'
     var n = element.appendChild(document.createElement("input"));
     n.className = 'n_input'
-    var dataset = element.appendChild(document.createElement("label"));
-    dataset.innerHTML = `    rows to remaining tables`
-    dataset.className = 'add_label2'
-    
-    var dataset = element.appendChild(document.createElement("BUTTON"));
-    dataset.innerHTML = `Make Data`
-    dataset.className = 'btn-primary'
+    var label2 = element.appendChild(document.createElement("label"));
+    label2.innerHTML = `    rows to remaining tables`
+    label2.className = 'add_label2'
+
+    var makedbutton = element.appendChild(document.createElement("BUTTON"));
+    makedbutton.innerHTML = `Make Data`
+    makedbutton.className = 'btn-primary'
   },
   updateAsync: function(data, element, config, queryResponse, details, done) {
     this.clearErrors();
@@ -73,20 +73,20 @@ looker.plugins.visualizations.add({
       var mb = document.querySelector('.modelbogus_input').value
       var rm = document.querySelector('.remaining_input').checked
       var n = document.querySelector('.n_input').value
-      package = {}
-      package['filename'] = fn
-      package['dataset'] = ds
-      package['mode'] = dp
-      modelb = mb == "" ? {} : JSON.parse(mb)
+      var packag = {}
+      packag['filename'] = fn
+      packag['dataset'] = ds
+      packae['mode'] = dp
+      modelb = mb === "" ? {} : JSON.parse(mb)
       if (rm) {
         modelb["."] = parseInt(n)
       }
-      package['modelbogus'] = modelb
-      console.log(package)
+      packag['modelbogus'] = modelb
+      console.log(packag)
       $.ajax({
-        type: "POST",
+        type: "GET",
         url: "https://us-central1-spencer-white-tckt87992.cloudfunctions.net/makedata",
-        data: package,
+        data: packag,
         success: console.log("Good")
       });
     });
