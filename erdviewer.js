af433311c3d17866cce49f35e32e0c652926645e
Noah MacDonald <noah.macdonald@looker.com>
2020-03-26 18:18:42 +0000

    adding erd viewer

--- erdviewer.js
+++ erdviewer.js
@@ -4,7 +4,7 @@ const visObject = {
   create: function(element, config){
   },
   updateAsync: function(data, element, config, queryResponse, details, doneRendering){
-    element.innerHTML = '<iframe width="100%" height="100%" src="https://docs.google.com/presentation/d/189zzC2dxr0TVRig8Tutgqd7uXdwfxRuQvm2bE8lqXyc/edit?usp=sharing"></iframe>';
+    element.innerHTML = '<iframe width="100%" height="100%" src="https://docs.google.com/presentation/d/189zzC2dxr0TVRig8Tutgqd7uXdwfxRuQvm2bE8lqXyc/edit#slide=id.g579ee364a7_0_0"></iframe>';
     doneRendering()
   }
 };
